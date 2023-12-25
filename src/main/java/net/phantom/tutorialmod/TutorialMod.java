e493ea5170050665680f7bba916b5f0ec6f2924a
Phantom447 <koo011707@gmail.com>
2023-12-24 22:07:00 -0800

    item group stuff

--- src/main/java/net/phantom/tutorialmod/TutorialMod.java
+++ src/main/java/net/phantom/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.phantom.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.phantom.tutorialmod.item.ModItemGroups;
+import net.phantom.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,10 +13,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
